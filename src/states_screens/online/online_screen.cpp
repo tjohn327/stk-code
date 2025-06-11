0d0aa3a7836b06199a44b28fb03a5ae4fe9134b6
Robin <unwi567@o2online.de>
2025-06-11 21:05:11 +0200

    Attempt III: Fix for manual server address adding.

--- src/states_screens/online/online_screen.cpp
+++ src/states_screens/online/online_screen.cpp
@@ -239,6 +239,17 @@ void OnlineScreen::onUpdate(float delta)
     m_online->setLabel(PlayerManager::getCurrentOnlineId() ? m_online_string
                                                            : m_login_string);
     // In case for entering server address finished
+    if (m_entered_server)
+    {
+        NetworkConfig::get()->setIsLAN();
+        NetworkConfig::get()->setIsServer(false);
+        ServerConfig::m_private_server_password = "";
+        STKHost::create();
+        NetworkingLobby::getInstance()->setJoinedServer(m_entered_server);
+        m_entered_server = nullptr;
+     //   StateManager::get()->resetAndSetStack(
+     //       NetworkConfig::get()->getResetScreens(true/*lobby*/).data());
+    }
 
     if (m_entered_server && !m_waiting_for_demo_name)
     {
@@ -252,8 +263,12 @@ void OnlineScreen::onUpdate(float delta)
             m_waiting_for_demo_name = true;
             new NetworkNameDialog([this](const core::stringw& name) {
                 onDemoNameEntered(name);
+                StateManager::get()->resetAndSetStack(
+                    NetworkConfig::get()->getResetScreens(true/*lobby*/).data());
             }, [this]() {
                 onDemoNameCancelled();
+                StateManager::get()->resetAndSetStack(
+                    NetworkConfig::get()->getResetScreens(true/*lobby*/).data());
             });
         }
         else
@@ -262,17 +277,6 @@ void OnlineScreen::onUpdate(float delta)
             // Normal connection flow
             proceedWithConnection();
         }
-    }
-    if (m_entered_server)
-    {
-        NetworkConfig::get()->setIsLAN();
-        NetworkConfig::get()->setIsServer(false);
-        ServerConfig::m_private_server_password = "";
-        STKHost::create();
-        NetworkingLobby::getInstance()->setJoinedServer(m_entered_server);
-        m_entered_server = nullptr;
-        StateManager::get()->resetAndSetStack(
-            NetworkConfig::get()->getResetScreens(true/*lobby*/).data());
     }
 }   // onUpdate
 
